8a3445e206ef92b20d98cf791fde296e862f7568
mehermrudula ambatipati <mehermrudula ambatipati>
2016-09-16 09:12:18 +0530

    files modified

--- vehicleDeals/app/register/register-controller.js
+++ vehicleDeals/app/register/register-controller.js
@@ -1,16 +1,74 @@
-(function(){
+(function () {
     'use strict'
     //code goes here.
     angular.module("register")
         .controller("registerCtrl",
-                    function($scope){
-          console.log("welcome to the controller");
-        $scope.heading ="Register";
+            function ($scope) {
+                $scope.userDetails = {
+                    terms: false
+                };
+                $scope.countries = [{
+                        "key": "IN",
+                        "value": "India"
+                    },
+                    {
+                        "key": "US",
+                        "value": "United States"
+                    }];
         
-        $scope.user="kiran";
+                var states = [{
+                        "countryCode": "IN",
+                        "key": "TG",
+                        "value": "Telangana"
+                    },
+                    {
+                        "countryCode": "IN",
+                        "key": "AP",
+                        "value": "Andhra Pradesh"
+                    },
+                    {
+                        "countryCode": "US",
+                        "key": "TX",
+                        "value": "Texas"
+                    }, {
+                        "countryCode": "US",
+                        "key": "NY",
+                        "value": "New York"
+                    }];
         
-        $scope.registerUser=function(){
-            console.log($scope)
+          $scope.states = [{
+                        "countryCode": "IN",
+                        "key": "TG",
+                        "value": "Telangana"
+                    },
+                    {
+                        "countryCode": "IN",
+                        "key": "AP",
+                        "value": "Andhra Pradesh"
+                    },
+                    {
+                        "countryCode": "US",
+                        "key": "TX",
+                        "value": "Texas"
+                    }, {
+                        "countryCode": "US",
+                        "key": "NY",
+                        "value": "New York"
+                    }];
+            
+        $scope.loadStates=function(){
+            console.log($scope.selectedCountry);
+            
+            $scope.stateList=[]; angular.forEach(states,function(item){
+                if(item.countryCode===$scope.selectedCountry.key){
+                   $scope.stateList.push(item); 
+                }
+            });
+            console.log($scope.stateList);
         };
-    });
+
+                $scope.registerUser = function () {
+                    console.log($scope.userDetails)
+                };
+            });
 })();
